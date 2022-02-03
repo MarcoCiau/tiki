02fdddcdcce42e2c8988ea4e705f69a77e295b20
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-03 10:20:53 -0600

    server added

---
+++ server/server.ts
@@ -0,0 +1,46 @@
+import express, { Application } from 'express';
+import envConfig from '../config/config';
+// import connectDB from '../config/db';
+import cors from 'cors';
+import deviceRoutes from '../routes/device';
+class Server {
+    private app: Application;
+    private port: string;
+    constructor() {
+        this.app = express();
+        this.port = envConfig.SERVER_PORT || '8000';
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        /* enable cors */
+        this.app.use(cors());
+        /* parse application/json requests*/
+        this.app.use(express.json());
+        /* parse application/x-www-form-urlencoded requests. Only parse string or arrays*/
+        this.app.use(express.urlencoded({ extended: false }));
+    }
+
+    routes() {
+        this.app.use('/api/v1/device', deviceRoutes);
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port: ${this.port}`);
+        });
+    }
+
+    async init() {
+        try {
+            // const isDBConnected = await connectDB();
+            // console.log('DB Connected!');
+            this.listen();
+        } catch (error) {
+            console.log('Init Server Failed : ', error);
+        }
+    }
+}
+
+export default Server;
