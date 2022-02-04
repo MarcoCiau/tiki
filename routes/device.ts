911c333cd8862ef6b58ea77b08840fdbd47b268c
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-04 11:20:11 -0600

    usage of device's middleware validators

--- routes/device.ts
+++ routes/device.ts
@@ -1,23 +1,13 @@
 import { Router, Request, Response } from "express";
 import * as deviceController from "../controllers/device";
 import * as authValidator from '../middlewares/auth.validator';
+import * as deviceValidator from '../middlewares/device.validator';
 const router = Router();
 
-// router.get('/', authValidator.validateJWT, (req: Request, res: Response) => {
-//     console.log(res.locals);
-//     console.log(res.locals.jwtPayload);
-//     res.status(200).json({ msg: 'Hello World' });
-// });
-
-// router.post('/', (req: Request, res: Response) => {
-//     console.log(req.body);
-//     res.status(200).json({ msg: 'OK' });
-// });
-
 router.get('/', authValidator.validateJWT, deviceController.getDevices);
-router.get('/:id', authValidator.validateJWT, deviceController.getDevice);
-router.post('/', authValidator.validateJWT, deviceController.createDevice);
-router.put('/:id', authValidator.validateJWT, deviceController.updateClient);
-router.delete('/:id', authValidator.validateJWT, deviceController.deleteClient);
+router.get('/:id', authValidator.validateJWT, deviceValidator.mongoIdRule(), deviceValidator.result, deviceController.getDevice);
+router.post('/', authValidator.validateJWT, deviceValidator.createRules(), deviceValidator.result, deviceController.createDevice);
+router.put('/:id', authValidator.validateJWT, deviceController.updateDevice);
+router.delete('/:id', authValidator.validateJWT, deviceValidator.mongoIdRule(), deviceValidator.result, deviceController.deleteDevice);
 
 export default router;
