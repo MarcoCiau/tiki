aa8072584cbe2bbc761bce77c85fc2059f28f227
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-09 13:43:21 -0600

    read route WIP

--- routes/reads.ts
+++ routes/reads.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
-// import * as deviceController from "../controllers/device";
 import * as readsController from "../controllers/reads";
 import * as authValidator from '../middlewares/auth.validator';
-import * as deviceValidator from '../middlewares/device.validator';
+import * as readValidator from '../middlewares/read.validator';
 const router = Router();
 
-router.get('/', authValidator.validateJWT, readsController.getReads);
-router.get('/:id', authValidator.validateJWT, readsController.getRead);
-router.post('/', authValidator.validateJWT, readsController.createRead);
-router.put('/:id', authValidator.validateJWT, deviceValidator.result, readsController.updateRead);
+router.get('/', authValidator.validateJWT, readValidator.getAllRules(), readValidator.result, readsController.getReads);
+router.get('/:id', authValidator.validateJWT, readValidator.mongoIdRule(), readValidator.result, readsController.getRead);
+router.post('/', authValidator.validateJWT, readValidator.createRules(), readValidator.result, readsController.createRead);
+router.put('/:id', authValidator.validateJWT, readValidator.result, readsController.updateRead);
 router.delete('/:id', authValidator.validateJWT, readsController.deleteRead);
 
 export default router;
