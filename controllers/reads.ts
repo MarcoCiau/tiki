7e8714cf30f356e6e700220cd58adb934fffe766
MarcoCiau <marcoantoniociau@hotmail.com>
2022-02-09 13:42:39 -0600

    read controllers WIP

---
+++ controllers/reads.ts
@@ -0,0 +1,88 @@
+import { Request, Response } from 'express';
+import ReadsModel from '../models/reads';
+import DeviceModel from '../models/device';
+
+export const getReads = async (req: Request, res: Response) => {
+    const {from = 0, limit = 5} = req.query;
+    try {
+        const fromNumber = Number(from);
+        const limitNumber = Number(limit);
+        const [count, reads] = await Promise.all([
+            ReadsModel.countDocuments(),
+            ReadsModel.find()
+            .skip(Number(fromNumber))
+            .limit(Number(limitNumber))
+        ]);
+        res.json({ msg: 'success', count, reads });
+    } catch (error) {
+        console.log('Get all Reads failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' })
+    }
+}
+
+export const getRead = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const result = await ReadsModel.findOne({ _id: id });
+        if (!result) {
+            return res.status(400).json({ msg: 'Read doesn\'t exists' });
+        }
+        res.status(200).json({ msg: 'success', device: result });
+    } catch (error) {
+        console.log('Get client failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' })
+    }
+}
+
+export const createRead = async (req: Request, res: Response) => {
+    try {
+        const {token, timestamp, metadata } = req.body;
+        const deviceExists = await DeviceModel.findOne({ token});
+        if (!deviceExists) 
+        {
+            return res.status(400).json({ msg: 'Device doesn\'t exists.' });
+        }
+        const readDocument = new ReadsModel({
+            deviceId : deviceExists._id,
+            timestamp: new Date(timestamp * 1000), 
+            metadata
+        });
+        const result = await readDocument.save();
+        res.status(200).json({ msg: 'success', read: result });
+    } catch (error) {
+        console.log('Create Device failed.', error);
+        res.status(500).json({ msg: 'something went wrong.' })
+    }
+}
+
+export const updateRead = async (req: Request, res: Response) => {
+    res.status(200).json({ msg: 'updateRead'});
+    // try {
+    //     const { id } = req.params;
+    //     const body = req.body;
+        
+    //     const result = await DeviceModel.findOneAndUpdate({ _id: id }, { ...body }, { new: true });
+    //     if (!result) {
+    //         return res.status(400).json({ msg: 'Device doesn\'t exists' });
+    //     }
+    //     res.status(200).json({ msg: 'success', device: result });
+    // } catch (error) {
+    //     console.log('update device failed.', error);
+    //     res.status(500).json({ msg: 'something went wrong.' });
+    // }
+}
+
+export const deleteRead = async (req: Request, res: Response) => {
+    res.status(200).json({ msg: 'deleteRead'});
+    // try {
+    //     const { id } = req.params;
+    //     const result = await DeviceModel.findByIdAndDelete({ _id: id });
+    //     if (!result) {
+    //         return res.status(400).json({ msg: 'Device doesn\'t exists' });
+    //     }
+    //     res.status(200).json({ msg: 'success', device: result });
+    // } catch (error) {
+    //     console.log('Delete Device failed.', error);
+    //     res.status(500).json({ msg: 'something went wrong.' });
+    // }
+}
