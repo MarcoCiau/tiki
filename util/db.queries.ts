34dfa7e3663420b13d26edf2fe3566de037ffdf3
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-15 18:04:04 -0500

    custom aggregation for reads

--- util/db.queries.ts
+++ util/db.queries.ts
@@ -1,12 +1,14 @@
 import ReadsModel from "../models/reads";
+import { Types } from 'mongoose';
+import { sensorType } from "./read.types";
 
 export const executeReadsQuery = (query: string, from: number = 0, limit: number = 5, sort: number = 1) => {
     return new Promise((resolve, reject) => {
-        let queryObj:object = {};
+        let queryObj: object = {};
         if (query === "") {
-            reject({error: "invalid query object"});
+            reject({ error: "invalid query object" });
         }
-        queryObj = {...JSON.parse(query)};
+        queryObj = { ...JSON.parse(query) };
         ReadsModel.find(queryObj)
             .skip(from)
             .limit(limit)
@@ -20,4 +22,19 @@ export const executeReadsQuery = (query: string, from: number = 0, limit: number
             });
 
     });
+}
+
+export const aggregateReads = async (device: Types.ObjectId, sensor: sensorType, minDate: Date, maxDate:Date) => {
+    return await ReadsModel.aggregate([
+        { $unwind: "$metadata" },
+        { $match: { $and: [{ "deviceId": device }, { "metadata.type": sensor }] } },
+        // { $match: { $and: [{ "deviceId": device }, { "metadata.type": sensor }, { timestamp: { $gte: new Date('2022-07-15T23:49:50.000Z'), $lte: new Date('2022-07-15T23:59:50.000Z') } }] } },
+        { $limit : 120 },
+        { $sort: {timestamp: -1}},
+        {
+            $group: {
+                _id: { timestamp: "$timestamp",value: "$metadata.value" },
+            },
+        },
+    ])
 }
