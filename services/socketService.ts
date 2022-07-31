6de06132827c53c96154c02c1edb12898f7c5bb3
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-31 13:45:39 -0500

    socket service and interface

---
+++ services/socketService.ts
@@ -0,0 +1,20 @@
+import server from "../server/server"
+import { Reads } from "../util/readModel.types";
+import { readsGetObj } from "../util/readModel.types";
+export const reportData = (payload: Reads) => {
+    
+    const reads :readsGetObj = {
+        deviceId: payload.deviceId,
+        timestamp: payload.timestamp,
+        pf: payload.metadata[4].value,
+        frequency: payload.metadata[5].value,
+        power: payload.metadata[3].value,
+        energy: payload.metadata[2].value,
+        lineVoltage: payload.metadata[1].value,
+        lineCurrent: payload.metadata[0].value,
+    }
+    console.log(reads);
+    
+    const socket = server.getSocketServer();
+    socket.sockets.in("abc123").emit("deviceSensors", JSON.stringify(reads));
+}
--- util/socket.types.ts
+++ util/socket.types.ts
@@ -1,11 +1,14 @@
 export interface ServerToClientEvents {
     noArg: () => void;
     basicEmit: (a: number, b: string, c: Buffer) => void;
+    deviceSensors: (payload: string) => void;
     withAck: (d: string, callback: (e: number) => void) => void;
 }
 
 export interface ClientToServerEvents {
     hello: () => void;
+    handShake: (deviceId: Object) => void;
+    room: (userId: string) => void;
 }
 
 export interface InterServerEvents {
