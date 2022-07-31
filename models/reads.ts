4f11ca9eb06ef1244473799e11076c3d49acc421
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-31 13:43:45 -0500

    Read interfaces

--- models/reads.ts
+++ models/reads.ts
@@ -1,18 +1,18 @@
 import { Schema, model, Types } from "mongoose";
 import { sensorType, unitType } from "../util/read.types";
+import { Reads, SensorData } from "../util/readModel.types";
+// interface sensorData {
+//     _id: Types.ObjectId,
+//     type: sensorType,
+//     value: number,
+//     unit: unitType
+// }
 
-interface sensorData {
-    _id: Types.ObjectId,
-    type: sensorType,
-    value: number,
-    unit: unitType
-}
-
-interface Reads {
-    deviceId: Types.ObjectId,
-    timestamp: Date,
-    metadata: sensorData[]
-}
+// interface Reads {
+//     deviceId: Types.ObjectId,
+//     timestamp: Date,
+//     metadata: sensorData[]
+// }
 
 const readSchema: Schema<Reads> = new Schema({
     deviceId: {
@@ -25,7 +25,7 @@ const readSchema: Schema<Reads> = new Schema({
         type: Date,
         index: true,
     },
-    metadata: [new Schema<sensorData>({
+    metadata: [new Schema<SensorData>({
         type: {
             type: String,
             default: sensorType.curr1,
---
+++ util/readModel.types.ts
@@ -0,0 +1,37 @@
+import {  Types } from "mongoose";
+import { sensorType, unitType } from "../util/read.types";
+
+
+export interface SensorData {
+    _id: Types.ObjectId,
+    type: sensorType,
+    value: number,
+    unit: unitType
+}
+
+export interface Reads {
+    deviceId: Types.ObjectId,
+    timestamp: Date,
+    metadata: SensorData[]
+}
+
+
+interface sensorDataset {
+    timestamp: Date | number,
+    value: number,
+}
+
+export interface readsGetObj {
+    deviceId: Types.ObjectId,
+    timestamp?: Date | number,
+    pf?: number,
+    frequency?: number,
+    power?: number,
+    energy?: number,
+    lineVoltage?: number,
+    lineCurrent?: number,
+    current?: sensorDataset[],
+    voltage?: sensorDataset[],
+    activeKwh?: sensorDataset[],
+    frequencyTS?: sensorDataset[]
+}
