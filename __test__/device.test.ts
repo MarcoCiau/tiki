3582e40ab593a12ba2c82aef32e55ac7e731d8cf
MarcoCiau <marcoantoniociau@hotmail.com>
2023-01-08 13:11:42 -0600

    device tests WIP, creating user done

--- __test__/device.test.ts
+++ __test__/device.test.ts
@@ -1,57 +1,49 @@
 import request from "../config/testConfig";
 import { connectDB, disconnectDB } from "../config/db";
+import DeviceModel, { Device } from "../models/device";
+import UserModel from "../models/user";
+import { deviceType } from "../util/read.types";
+import { signupUser } from "./factories/authFactory";
+
+//params
 const deviceURLBase: string = "/api/v1/device";
 let accessToken: string = "*";
-beforeAll(() => jest.setTimeout(90 * 1000));
-describe("DB Connection", () => {
-    test("It should return true if database is connected successfully", async () => {
-        const db = await connectDB();
-        expect(db).toBeTruthy();
-    });
-});
+//data 
+let newDevice: Partial<Device> = {
+    name: "deviceTest01",
+    mac: "FF:FF:FF:FF:FF:FF",
+    type: deviceType.singlePhase
+}
 
-
-// new device test
 describe("Creating a new device - Test ", () => {
-    test("Signin Success - It should respond with user payload, accessToken & refreshToken", async () => {
-        const response = await request
-            .post("/api/v1/auth/signin")
-            .send({
-                email: "testuser3@email.com",
-                password: "qwertyui8"
-            });
 
-        expect(response.statusCode).toBe(200);
-        expect(response.body.msg).toBe("success");
-        expect(response.body.accessToken).not.toBeNull();
-        expect(response.body.refreshToken).not.toBeNull();
+    beforeAll(async () => {
+        jest.setTimeout(100 * 1000);
+        await connectDB();
+    })
 
-        accessToken = response.body.accessToken;
-        console.log(accessToken);
-
+    afterAll(async () => {
+        await disconnectDB();
     });
 
-    test("Missed access token header - It should respond with an 401 request", async () => {
-        const response = await request
-            .post(deviceURLBase)
-            .send({
-                name: "",
-                mac: "",
-                type: ""
-            });
-        expect(response.statusCode).toBe(401);
-    });
+    // Cleans up database between each test
+    afterEach(async () => {
+        await UserModel.deleteMany()
+        await DeviceModel.deleteMany()
+    })
 
-    test("Invalid access token value - It should respond with an 401 request", async () => {
+    test("Missed access token header - It should respond with an 400 request", async () => {
         const response = await request
             .post(deviceURLBase)
-            .send({
-                name: "",
-                mac: "",
-                type: ""
-            })
+            .send(newDevice);
+        expect(response.statusCode).toBe(400);
+    });
+    test("Invalid access token value - It should respond with an 400 request", async () => {
+        const response = await request
+            .post(deviceURLBase)
+            .send(newDevice)
             .set("x-token", "dfdfdf4i45i45");
-        expect(response.statusCode).toBe(401);
+        expect(response.statusCode).toBe(400);
     });
 
     test("Empty Request's body - It should respond with a bad request", async () => {
@@ -77,33 +69,18 @@ describe("Creating a new device - Test ", () => {
     test("Invalid device Type - It should respond with an bad request", async () => {
         const response = await request
             .post(deviceURLBase)
-            .send({
-                name: "myDevice",
-                mac: "74:69:69:2D:30:02",
-                type: "7"
-            })
+            .send({ ...newDevice, type: "7" })
             .set("x-token", accessToken);
         expect(response.statusCode).toBe(400);
     });
 
     test("Device Created - It should respond the new refresh token & access token", async () => {
+        const auth = await signupUser();
+        accessToken = auth.body.accessToken;
         const response = await request
             .post(deviceURLBase)
-            .send({
-                name: "myDevice1245",
-                mac: "74:69:69:2D:30:08",
-                type: 1
-            })
+            .send(newDevice)
             .set("x-token", accessToken);
-        expect(response.statusCode).toBe(200);
-        expect(response.body.msg).toBe("success");
-        expect(response.body.device).not.toBeNull();
-    });
-});
-
-describe("DB Disconnection", () => {
-    test("It should return true if database is disconnected successfully", async () => {
-        const disconnected = await disconnectDB();
-        expect(disconnected).toBeTruthy();
+        expect(response.statusCode).toBe(201);
     });
 });
