95ccd576c5a43b4341c9a821238138ef9179f645
MarcoCiau <marcoantoniociau@hotmail.com>
2023-01-16 08:45:59 -0600

    device sort options defined in an enum

--- util/device.quety.types.ts
+++ util/device.quety.types.ts
@@ -2,3 +2,10 @@ export  enum deviceConnectionStatus {
     disconnected = "disconnected",
     connected = "connected",
 }
+
+export enum deviceSortOptions {
+    latest = "latest",
+    oldest = "oldest",
+    asc = "a-z",
+    desc = "z-a"
+}
